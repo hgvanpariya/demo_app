70ccf2c1a9e8f7b3a797e72c0fafbbf2910ea9e5
hari <h.g.vanpariya@gmail.com>
2012-02-23 00:08:39 +0530

    anothe rproject

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'ff52af195c5906b6b3ec716c52a2e444b436f55bb9a77136b216de1f355f74edb8e931050c1d0fbc03a923278309a7274405bb208a57dcf4a6e9cc29bb549ba8'
